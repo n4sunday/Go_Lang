308de1573d2eb3375ad5d5cf48287dfb65b74005
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-15 02:44:05 +0700

    Variable

--- hello.go
+++ hello.go
@@ -2,4 +2,14 @@ package main
 import ("fmt")
 func main() {
 	fmt.Println("Hello World Go Lang");
+	var myAge int = 22
+	myAge2 := 500.3
+	var something bool=true
+	text := "Sunday"
+	age1,age2:=35,40
+	fmt.Println("Age : ",myAge)
+	fmt.Println("Age 2 : ",myAge2)
+	fmt.Println("Boolean : ",something)
+	fmt.Println("Name : ",text)
+	fmt.Println(age1,age2)
 }
