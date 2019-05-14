c7bc56e0cdc7e932bbfbf0c08f00c4b3b2a1314d
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-15 03:32:26 +0700

    If Else Statment

--- hello.go
+++ hello.go
@@ -4,12 +4,27 @@ import (
 	"fmt"
 )
 
-//Input Scanf
+//If Else Statment
 func main() {
 	fmt.Println("Input You Number:")
 	var input float64
 	fmt.Scanf("%f", &input)
-	output := input * 10
-	fmt.Println("Sum:", output)
+	condition := input > 2
+	if condition {
+		fmt.Println("Worked")
+	} else {
+		fmt.Println("Not Worked")
+	}
+
+	if 6 > 3 && 8 > 5 {
+		fmt.Println("Worked 6>3 && 8>5")
+	} else {
+		fmt.Println("Not Worked")
+	}
+
+	if 6 > 3 || 3 > 5 {
+		fmt.Println("Worked 6>3 || 8>5")
+	} else {
+	}
 
 }
