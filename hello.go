37fcbe5e00f2ab2df5feeca27976f8a6c7a77f45
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-15 02:54:31 +0700

    String

--- hello.go
+++ hello.go
@@ -1,15 +1,24 @@
 package main
-import ("fmt")
+
+import (
+	"fmt"
+)
+
 func main() {
-	fmt.Println("Hello World Go Lang");
-	var myAge int = 22
-	myAge2 := 500.3
-	var something bool=true
-	text := "Sunday"
-	age1,age2:=35,40
-	fmt.Println("Age : ",myAge)
-	fmt.Println("Age 2 : ",myAge2)
-	fmt.Println("Boolean : ",something)
-	fmt.Println("Name : ",text)
-	fmt.Println(age1,age2)
+	//Operater Math
+	fNumber := 60
+	fSecond := 5
+	fmt.Println(fNumber + fSecond)
+	fmt.Println(fNumber - fSecond)
+	fmt.Println(fNumber * fSecond)
+	fmt.Println(fNumber / fSecond)
+	//Strung
+	p1 := "Nattapon"
+	p2 := "Sunday"
+	//Concatenation
+	p3 := p1 + p2
+	fmt.Println(p3)
+	fmt.Println(p3[0:3]) //Nat
+	fmt.Println(p3[0:])  //NattaponSunday
+	fmt.Println(p3[0])   //Ascii
 }
