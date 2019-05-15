d28332e0f51b9ee1021149d4ddbc8bf4b7f9f929
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-16 03:08:17 +0700

    String Function

--- hello.go
+++ hello.go
@@ -1,16 +1,46 @@
 package main
 
-import ("fmt")
-
-//Go Routine Function
-
+import "fmt"
+import s "strings"
+//String Function
+var p = fmt.Println
 func main() {
-	go f(0)
-	var input string
-	fmt.Scanln(&input)
+	p("Contains", s.Contains("Test", "es")) //มี es อยู่ใน Test รึป่าว
+	p("Count", s.Count("Test", "t")) //นับ
+	p("HasPrefix", s.HasPrefix("Lord Varder", "Lord"))
+	p("HasSuffix", s.HasSuffix("Lord Varder", "Varder"))
+	p("Index", s.Index("Lord Varder", "V"))
+	p("Join", s.Join([]string{"a","b","c"},"-"))
+	p("Repeat", s.Repeat("a", 5)) //ทำซ้ำ
+	p("Replace", s.Replace("foo", "o", "0", -1))// o เป็น 0 ตำแหน่ง -1
+	p("Replace", s.Replace("fooo", "o", "0", 2)) //แทน O ด้วยเลข 0 ถ้าพบ 2 แทน 2 ตัวเลย
+	p("Split", s.Split("a-b-c-d-e", "-"))
+	p("ToLower", s.ToLower("Nattapon"))
+	p("ToUpper", s.ToUpper("Nattapon"))
+	p("Len", len("Nattapon"))
+
+
+
+
+
+
+
+
+
+
+
 }
-func f(n int){
-	for i:=0; i<100; i++{
-		fmt.Println(n,":",i)
-	}
-}
+
+/*
+	Contains 
+	Count
+	HasPrefix
+	HasSuffix
+	Index
+	Join
+	Repeat
+	Replace
+	Split
+	ToLower
+	ToUpper
+*/
