b8b1c879e6142824aa9bc906d3e1a5df0d0309bf
s5935512004 <s5935512004@phuket.psu.ac.th>
2019-05-16 02:14:28 +0700

    Map

--- hello.go
+++ hello.go
@@ -2,22 +2,22 @@ package main
 
 import ("fmt")
 
-//Slice
+//Map
 func main() {
-	x:=make([]float64,5) //Slice 5 
+	/*x:=make(map[string] string)
+	x["TH"]="Thailand"
+	x["JP"]="Japan"
+	x["EN"]="England"
+	x["H"]="Hydrogen"
+	x["Li"]="Lithium"*/
+	x:=map[string] string{
+		"TH":"Thailand",
+		"JP":"Japan",
+	}
+
+	//fmt.Println(x["EN"])
+	//fmt.Println(x["Li"])
 	fmt.Println(x)
 
-	slice1:=[]int{1,2,3,4,5,6,7,8}
-	slice2:=append(slice1,9,10)
-	fmt.Println(slice1)
-	fmt.Println(slice2)
 
-	arr:=[5]float64{1,2,3,4,5}
-	y:=arr[0:4]
-	fmt.Println(y) 
-
-	slice3:=[]int{10,20,30}
-	slice4:=make([]int,2)
-	copy(slice4,slice3)
-	fmt.Println(slice4)
 }
