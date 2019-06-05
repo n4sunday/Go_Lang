38416560b8c5827715834d8016f75ba45d822b32
n4s <s5935512004@phuket.psu.ac.th>
2019-06-06 02:47:58 +0700

    for no assign var

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
-package main
-import (
-	"fmt"
-)
+package main 
+import ( "fmt")
 
-func spit(num int)(x int, y int){
-	x = num * 5 / 2
-	y = num - 10
-	return 
+func main(){
+	sum := 1
+	for ;sum<20; {
+		sum += sum
+		fmt.Println(sum)
+	}	
 }
-
-func main() {
-	fmt.Println(spit(20))
-}
-
