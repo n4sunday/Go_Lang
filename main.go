653048feae4c785a3be27cc7eef2a80948cedfe7
n4s <s5935512004@phuket.psu.ac.th>
2019-06-07 02:37:21 +0700

    struct fields

--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
-import ("fmt")
+import "fmt"
 
-func main(){
-	i, j := 5, 10
+type Vertex struct {
+	X int
+	Y int
+	Z string
+	A float64
+	B bool
+}
+type Num struct {
+	X int
+	Y int
+}
 
-	p := &i				 //point to i
-	fmt.Println(*p) //5  //read i through the poiner
-	*p = 4				 //set i through the pointer
-	fmt.Println(i)	//4	 //see the new value of i
+func main() {
+	fmt.Println(Vertex{1,2,"Sun",3.14,true})
 
-	p = &j				 //point to j
-	*p = *p/5 			 //divide j through the pointer
-	fmt.Println(j)  //2	 //see the new value of j
+	v := Num{10, 20}
+	fmt.Println(v.X)
+	fmt.Println(v.Y)
 }
