08f89ab84615c807ef66fc31038f7e3d09e5d9ab
n4s <s5935512004@phuket.psu.ac.th>
2019-06-07 02:45:32 +0700

    struct pointers

--- main.go
+++ main.go
@@ -1,23 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Vertex struct {
-	X int
-	Y int
-	Z string
-	A float64
-	B bool
-}
 type Num struct {
 	X int
 	Y int
 }
 
-func main() {
-	fmt.Println(Vertex{1,2,"Sun",3.14,true})
-
-	v := Num{10, 20}
-	fmt.Println(v.X)
-	fmt.Println(v.Y)
+func main(){
+	v := Num{10,20}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
 }
