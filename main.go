42650d01e4c0eab635d405339fbed7b0a9658396
n4s <s5935512004@phuket.psu.ac.th>
2019-06-07 01:44:41 +0700

    Defer

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
+
 import (
 	"fmt"
-	"time"
 )
-func main(){
-	t := time.Now()
-	fmt.Printf("Time now : %s\n",t)
-	switch {
-	case t.Hour() < 12:
-		fmt.Println("Good morning!")
-	case t.Hour() < 17:
-		fmt.Println("God afternoon.")
-	default:
-		fmt.Println("Good evning.")
-	}
+
+func main() {
+	defer fmt.Println("Nattapon")
+
+	fmt.Println("Hello")
 }
