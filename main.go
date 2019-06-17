d9c81814a1eb756a15968d059ccbe8ac586f5e7e
n4s <s5935512004@phuket.psu.ac.th>
2019-06-18 01:52:25 +0700

    array

--- main.go
+++ main.go
@@ -4,20 +4,13 @@ import (
 	"fmt"
 )
 
-type Num struct {
-	X int
-	Y int
+func main(){
+	var a [2]string
+	a[0] = "Hello"
+	a[1] = "Wolrd"
+	fmt.Println(a[0],a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
 }
-
-var (
-	v1 = Num{1 ,2} //has type Num
-	v2 = Num{X: 10} //Y:0 is imlicit
-	v3 = Num{}     //X:0 and Y:0
-	p = &Num{1,2}  //has type *Num
-)
-
-func main() {
-	fmt.Println(v1,p,v2,v3) //{1 2} &{1 2} {10 0} {0 0}
-}
-
-
