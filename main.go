16599f1681c3e1ce14c7fa81f3dde234ab7730ae
n4s <s5935512004@phuket.psu.ac.th>
2019-06-20 02:10:30 +0700

    range continued

--- main.go
+++ main.go
@@ -5,15 +5,13 @@ import (
 	"fmt"
 )
 
-var pow = []int{1, 2,4,8,16,32,64,128}
-var name = []string{"Sun","Rob","John"}
 
 func main(){
-	for i, v:=range pow {
-		fmt.Printf("2**%d = %d\n",i,v)
+	pow := make([]int,10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) //2**i
 	}
-
-	for i, x:=range name {
-		fmt.Printf("%d %s \n",i,x)
+	for _,value := range pow {
+		fmt.Printf("%d\n",value)
 	}
 }
