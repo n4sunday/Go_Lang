ac9304a0d34f639fc33c6ab3c04f1e9faae3c01a
n4s <s5935512004@phuket.psu.ac.th>
2019-06-21 02:06:25 +0700

    map literals

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
-import (
-	"fmt"
-)
+
+import "fmt"
+
 type Vertex struct {
 	Lat, Long float64
 }
 
-var m map[string]Vertex
+var m = map[string]Vertex{
+	"Bell Labs": Vertex {
+		40.68433,-74.2254,
+	},
+	"Google": Vertex {
+		37.42202,-122.08408,
+	},
+}
 
-func main(){	
-	m = make(map[string]Vertex)
-	m["Bell Labs"] = Vertex {
-		40.68433, -74.39967,
-	}
-	fmt.Println(m["Bell Labs"])
+func main(){
+	fmt.Println(m)
 }
